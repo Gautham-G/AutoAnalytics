a086a8393f9856110adeca999ac8281134c25d9f
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2023-03-01 17:40:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,45 +18,47 @@ if uploaded_file is not None:
   st.write('Your response variable is : ', response_col)
   
   data_columns = st.multiselect("Predictor variables (x)", df.columns)
-  
-  x = df[data_columns]
-  y = df[response_col]
-  
-  train = df.sample(frac=0.8,random_state=200)
-  test = df.drop(train.index)
+  if data_columns is not None:
+    x = df[data_columns]
+    y = df[response_col]
 
-  x_train = np.array(train[data_columns])
-  x_test = np.array(test[response_col])
-  
-  y_train = np.array(train[data_columns])
-  y_test = np.array(test[response_col])
-  
-  bias_col = np.ones((len(x_train), 1))
-#   st.write(bias_col.shape)
-#   st.write(x_train.shape)
-  x_train = np.hstack((bias_col, x_train))
-#   st.write(x_train.shape)
+    train = df.sample(frac=0.8,random_state=200)
+    test = df.drop(train.index)
 
-  b_hat = np.linalg.inv(x_train.T@x_train)@x_train.T@y_train
-#   st.write(b_hat[0], b_hat[1])
-  y_hat = b_hat[0] + b_hat[1]*x_test
+    x_train = np.array(train[data_columns])
+    x_test = np.array(test[response_col])
 
-  
-  fig, ax = plt.subplots()
-  
-  ax.plot(x_test, y_test, label = 'test truth')
-  ax.plot(x_test, y_hat, label = 'test pred')
-  ax.legend()
- 
-  st.pyplot(fig)
-  
-  rmse = np.linalg.norm(y_hat-y_test)
-  st.write('RMSE is ', rmse)
-  if(rmse<1e-5):
-    st.write('Woah! Thats a great model!')
+    y_train = np.array(train[data_columns])
+    y_test = np.array(test[response_col])
+
+    bias_col = np.ones((len(x_train), 1))
+  #   st.write(bias_col.shape)
+  #   st.write(x_train.shape)
+    x_train = np.hstack((bias_col, x_train))
+  #   st.write(x_train.shape)
+
+    b_hat = np.linalg.inv(x_train.T@x_train)@x_train.T@y_train
+  #   st.write(b_hat[0], b_hat[1])
+    y_hat = b_hat[0] + b_hat[1]*x_test
+
+
+    fig, ax = plt.subplots()
+
+    ax.plot(x_test, y_test, label = 'test truth')
+    ax.plot(x_test, y_hat, label = 'test pred')
+    ax.legend()
+
+    st.pyplot(fig)
+
+    rmse = np.linalg.norm(y_hat-y_test)
+    st.write('RMSE is ', rmse)
+    if(rmse<1e-5):
+      st.write('Woah! Thats a great model!')
+    else:
+      st.write('Bad model :(')
   else:
-    st.write('Bad model :(')
-    
+    st.write('Choose predictors!')
+
   
 #   choose alpha with st.slider
 #   display type of plot with st.multiselect
